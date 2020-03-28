b9d319778328af54a845112e5f0791a3907470dc
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-03-28 14:15:38 -0600

    organized files and created model

---
+++ TheMoviedbObj-C/Model/MDBMovie.h
@@ -0,0 +1,25 @@
+//
+//  MDBMovie.h
+//  TheMoviedbObj-C
+//
+//  Created by Kelsey Sparkman on 3/28/20.
+//  Copyright © 2020 Kelsey Sparkman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MDBMovie : NSObject
+
+@property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) NSNumber *rating;
+@property (nonatomic, readonly) NSString *overview;
+
+- (instancetype)initWithTitle:(NSString *)title rating:(NSNumber *)rating overview:(NSString *)overview;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
